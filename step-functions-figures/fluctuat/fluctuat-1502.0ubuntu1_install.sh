87e5e65c062984fee59645535b64734317c40500
Anthony Dario <adario@uoregon.edu>
2025-04-15 16:46:53 -0700

    Remove unecessary files
    - The license file is covered by Elaenia's license.
    - Fluctuat's .deb file is unecessary.
    - The data from the experiments can be regenerated.

--- step-functions-figures/fluctuat/fluctuat-1502.0ubuntu1_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-grep -q '^export FLUCTUATHOME=' ~/.bashrc || (echo 'export FLUCTUATHOME=/opt/fluctuat' >> ~/.bashrc; echo 'export PATH=$PATH:$FLUCTUATHOME/bin' >> ~/.bashrc)
-sudo apt install -y  libboost-filesystem1.74.0 libc6 libgcc-s1 libgl1 libglu1-mesa  libgmp10 libmpfr6 libqt5core5a libqt5gui5    libqt5opengl5 libqt5printsupport5 libqt5widgets5 libstdc++6 gcc original-awk sed
